17a96d17986cf3bb9e977dfa158b933b5840e9bc
m-r-r <raybaudroigm@gmail.com>
2015-10-19 02:23:06 +0200

    Markdown reader

---
+++ src/readers/markdown.rs
@@ -0,0 +1,58 @@
+use std::io::Read;
+use std::path::Path;
+use std::fs::File;
+use std::error::Error as StdError;
+use hoedown::{Markdown, Render, Wrapper, Html};
+use super::{Reader, Metadata};
+use super::super::{Error, Result};
+
+#[derive(Debug)]
+pub struct MarkdownReader;
+
+static EXTENSIONS: &'static [&'static str] = &["markdown", "md", "mkd", "mdown"];
+
+impl Reader for MarkdownReader {
+    fn extensions() -> &'static [&'static str] {
+        EXTENSIONS
+    }
+
+    fn load(&self, path: &Path) -> Result<(String, Metadata)> {
+        let mut fd = try! { File::open(path) };
+        let mut renderer = HtmlRender::new();
+        let input = Markdown::read_from(fd);
+        let body = String::from(try! { renderer.render(&input).to_str()
+            .map_err(|err| Error::Reader {
+                    path: path.into(),
+                    cause: Box::new(err),
+                }
+            )
+        });
+
+        unimplemented!();
+    }
+}
+
+
+struct HtmlRender {
+    base: Html,
+}
+
+impl HtmlRender {
+    fn new() -> HtmlRender {
+        use hoedown::renderer::html::Flags;
+
+        HtmlRender {
+            base: Html::new(Flags::empty(), 6),
+        }
+    }
+}
+
+impl Wrapper for HtmlRender {
+    type Base = Html;
+
+    fn base(&mut self) -> &mut Html {    
+        &mut self.base
+    }
+} 
+
+wrap!(HtmlRender);
---
+++ src/readers/mod.rs
@@ -0,0 +1,15 @@
+use std::io::Read;
+use std::path::Path;
+use super::Result;
+
+mod markdown;
+pub use self::markdown::MarkdownReader;
+
+pub type Metadata = ();
+
+pub trait Reader {
+    fn extensions() -> &'static [&'static str]
+        where Self: Sized;
+    fn load(&self, path: &Path) -> Result<(String, Metadata)>;
+}
+
