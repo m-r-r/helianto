98735b271ec0124558e79d97e0c0207204061e09
m-r-r <raybaudroigm@gmail.com>
2015-10-23 01:13:53 +0200

    Add metadata support to the Markdown reader
    The Markdown reader can now read key-value metadata embedded in the document :
    
    Example:
    
        # Document title
    
        Key1: Value 1
        Key2: Value 2
    
        Body
    
    The reader will return:
    
        ("<p>Body</p>", [("Key1", "Value 1"), ("Key2", "Value 2")])

--- src/readers/markdown.rs
+++ src/readers/markdown.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use std::fs::File;
-use hoedown::{Markdown, Render, Wrapper, Html};
+use regex::{Regex};
+use hoedown::{Markdown, Render, Wrapper, Html, Buffer};
 use super::{Reader, Metadata};
 use super::super::{Settings, Error, Result};
 
@@ -30,20 +31,41 @@ impl Reader for MarkdownReader {
             )
         });
 
-        Ok((body, Metadata::new()))
+        Ok((body, renderer.into()))
     }
 }
 
 
 struct HtmlRender {
     base: Html,
+    before_header: bool,
+    before_metadata: bool,
+    metadata: Metadata,
+    metadata_regex: Regex,
 }
 
 impl HtmlRender {
     fn new() -> HtmlRender {
         use hoedown::renderer::html::Flags;
 
-        HtmlRender { base: Html::new(Flags::empty(), 6) }
+        HtmlRender {
+            base: Html::new(Flags::empty(), 6),
+            before_header: true,
+            before_metadata: true,
+            metadata: Metadata::new(),
+            metadata_regex: Regex::new(r"(?m)\A[ \t\n]*((?:[ \t]*[^ \t\\:]+[ \t]*:[^\n]*[\n]*$)+)[ \t\n]*\z").unwrap(),
+        }
+    }
+
+    fn read_metadata(&mut self, buffer: &Buffer) {
+        if let Ok(metadata_str) = buffer.to_str() {
+            for line in metadata_str.lines() {
+                let parts: Vec<&str> = line.splitn(2, ":").collect();
+                if let (Some(key), Some(value)) = (parts.get(0), parts.get(1)) {
+                    self.metadata.insert(String::from(key.trim()), String::from(value.trim()));
+                }
+            }
+        }
     }
 }
 
@@ -52,6 +74,34 @@ impl Wrapper for HtmlRender {
 
     fn base(&mut self) -> &mut Html {
         &mut self.base
+    }
+
+    fn header(&mut self, ob: &mut Buffer, content: &Buffer, level: i32) {
+        if self.before_header {
+            if let Ok(title) = content.to_str().map(|s| s.trim()) {
+                self.metadata.insert("title".into(), title.into()); 
+                self.before_header = false;
+            }
+        } else {
+            self.base().header(ob, content, level);
+        }
+    }
+
+    fn paragraph(&mut self, ob: &mut Buffer, content: &Buffer) {
+        let is_metadata = content.to_str().ok()
+            .map(|s| self.metadata_regex.is_match(s))
+            .unwrap_or(false);
+        if !self.before_header && self.before_metadata && is_metadata {
+            self.read_metadata(content);
+        } else {
+            self.base().paragraph(ob, content);
+        }
+    }
+}
+
+impl Into<Metadata> for HtmlRender {
+    fn into(self) -> Metadata {
+        self.metadata
     }
 }
 
